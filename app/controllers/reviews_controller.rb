dd1f62f3a779fb68c4012b94c685c00aaf2618e0
Ralph Perdomo <ralph@prdmo.com>
2018-12-14 15:25:50 -0800

    add create ability to reviews and associate with products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,48 @@
 class ReviewsController < ApplicationController
-
+  before_action :get_product
 
   def new
-    binding.pry
-    @product = Product.find(params[:product_id].to_i)
+    # @product = Product.find(params[:product_id].to_i)
     @review = @product.reviews.new
 
   end
+
+  def create
+    if @review = @product.reviews.create(permit_params)
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+    # binding.pry
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def get_product
+    @product=Product.find(params[:product_id].to_i)
+  end
+
+  def permit_params
+    params.permit(:author, :content_body, :rating, :product_id)
+  end
+
 
 
 
